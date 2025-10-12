96ad7900615ff442a6572a3128d12ad477713581
roaming <wilhelmus4321@gmail.com>
2025-10-12 13:42:32 +0100

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
+#include <string>
 
-void getUsername(std::string& username)
+#include <curl/curl.h>
+
+std::string getUsername()
 {
+    std::string username {};
     std::cin >> username;
     std::cin.ignore();
+    return username;
 }
+
+
 
 int main()
 {
-    std::string username {};
+    std::cout << "Enter the github username: ";
+    std::string username = getUsername();
 
-    getUsername(username);
+    CURL* handle = curl_easy_init;
+    if (handle)
+    {
+        curl_easy_setopt(handle, CURLOPT_URL, "httos://api.github.com");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback); // error no writeCallback function
 
-    std::cout << username << "\n";
+        CURLcode res = curl_easy_perform(handle); // CURLcode is enum for error message - easy preform does https request
+    }
 }
